f343b19154fac43d632a07fd7b037c3cc94373d9
Pierdomenico Apruzzese <pieroapruzzese@gmail.com>
2021-05-23 23:58:43 +0200

    slight change

---
+++ src/components/Registration.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import {Form, FormGroup} from "react-bootstrap"
+
+export default class Registration extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
